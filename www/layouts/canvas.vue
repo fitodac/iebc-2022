1663b1e1bea6c8861fffe7f5396b59b90fd65f44
fitodac <fitodac@gmail.com>
2023-06-07 18:42:23 -0300

    Layouts

---
+++ www/layouts/canvas.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useStore } from '@/store/pagedata'
+
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Menu from '@/components/Menu'
+import Loader from '@/components/Loader'
+
+const store = useStore()
+
+const wrapper_class = computed(() => {
+	return 'bg-white min-h-screen antialiased'
+})
+
+onMounted(() => store.setLoading(false) )
+</script>
+
+<template>
+<div>
+	<div :class="wrapper_class">
+		<slot />
+		<Footer />
+	</div>
+
+	<Menu/>
+</div>
+</template>
+
+
+<style scoped>
+.wrapper_class{
+	@apply overflow-x-hidden;
+}
+</style>
---
+++ www/layouts/default.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { useStore } from '@/store/pagedata'
+
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Menu from '@/components/Menu'
+import Loader from '@/components/Loader'
+
+const store = useStore()
+
+const wrapper_class = computed(() => {
+	return 'bg-white min-h-screen antialiased'
+})
+
+
+onMounted(() => store.setLoading(false) )
+</script>
+
+<template>
+<div>
+  <div :class="wrapper_class">
+		<page-header />
+
+		<slot />
+
+		<Footer />
+  </div>
+
+	<transition>
+		<Menu/>
+	</transition>
+</div>
+</template>
+
+
+<style scoped>
+.wrapper_class{
+	@apply overflow-x-hidden;
+}
+</style>
